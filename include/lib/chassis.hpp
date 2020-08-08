d0ea92a0b51d7029d0a4cdfb5807fc3a23f25f3c
Nikhil Dave <nikhilrdave2003@gmail.com>
2020-08-08 07:57:49 -0700

    Added documentation + header for chassisPID

--- include/lib/chassis.hpp
+++ include/lib/chassis.hpp
@@ -18,11 +18,19 @@
 class Chassis {
     private:
         /**
-         * OkapiLib motor group objects. Each one represents one half of the base
+         * OkapiLib motor group objects. Each one represents the 
+         * motor(s) on one side of the base
          */
         okapi::MotorGroup rightBase;
 
         okapi::MotorGroup leftBase;
+
+        /**
+         * Variable to store the diameter of the wheel
+         * This value is used to convert values passed in for
+         * autonomous functions from inches to degrees to rotate for
+         */ 
+        float wheelDiameter;
     public:
         /**
          * One of the constructors for a chassis object. This one is for a tank
@@ -35,8 +43,10 @@ class Chassis {
          * rightReversed: a boolean to indicate if the right motor needs to be reversed
          * gearset: the motor gearset used in the motors (it is assumed that both 
          * motors use the same gearset)
+         * wD: the diameter of the wheels used
          */ 
-        Chassis(int leftMotor, int rightMotor, bool leftReversed, bool rightReversed, okapi::AbstractMotor::gearset gearset);
+        Chassis(int leftMotor, int rightMotor, bool leftReversed, bool rightReversed, 
+                okapi::AbstractMotor::gearset gearset, float wD);
         /** 
          * One of the constructors for a chassis object. This one is for a tank
          * drive with 2 motors powering each side
@@ -45,19 +55,44 @@ class Chassis {
          * leftFrontMotor: the motor port of the motor on the left side of the base
          * rightFrontMotor: the motor port of the motor on the right side of the base
          * leftBackMotor: the motor port of the motor on the left side of the base
+         * leftFrontReversed: a boolean to indicate if the left front motor needs to be reversed
+         * rightFrontReversed: a boolean to indicate if the right front motor needs to be reversed
+         * leftBackReversed: a boolean to indicate if the left back motor needs to be reversed
+         * rightBackReversed: a boolean to indicate if the right back motor needs to be reversed         
          * rightBackMotor: the motor port of the motor on the right side of the base
          * gearset: the motor gearset used in the motors (it is assumed that both 
          *          motors use the same gearset)
+         * wD: the diameter of the wheels used
          */ 
         Chassis(int leftFrontMotor, int rightFrontMotor, int leftBackMotor, int rightBackMotor, 
-                bool leftFrontReversed, bool rightFrontReversed, bool leftBackReversed, bool rightBackReversed,
-                okapi::AbstractMotor::gearset gearset);
+                bool leftFrontReversed, bool rightFrontReversed, 
+                bool leftBackReversed, bool rightBackReversed,
+                okapi::AbstractMotor::gearset gearset, float wD);
 
         /**
-         * The function to control the chassis during the opcontrol period
+         * The driver function allows control of the chassis during the opcontrol period,
+         * with each side of the base bound to one of the controller joysticks
          * 
          * Parameter:
          * controller: the OkapiLib controller object, which represents the robot controller
          */ 
         void driver(okapi::Controller controller);
+
+        /**
+         * The chassisPID function is a PID controller for the base. It sets each side of 
+         * the base to move a specified length. It is used as the base for all autonomous 
+         * functions. Individual functions conduct any calculations needed to get the 
+         * needed target values before passing those values into chassisPID. This means
+         * that the PID controller for the chassis need only be tuned once, but used in all
+         * autonomous functions
+         * 
+         * Parameters:
+         * leftTarg: The target length to move to, in inches, for the left side of the base
+         *           Can be negative to indicate rotating backwards
+         * rightTarg: The target length to move to, in inches, for the right side of the base
+         *           Can be negative to indicate rotating backwards
+         * maxVelo: The maximum velocity each motor group can reach. Used to limit the robot's
+         *          speed in autonomous, to minimize overshooting
+         */
+        void chassisPID(float leftTarg, float rightTarg, int maxVelo); 
 };
--- src/lib/chassis.cpp
+++ src/lib/chassis.cpp
@@ -8,23 +8,24 @@
 
 Chassis::Chassis(int leftMotor, int rightMotor, 
                  bool leftReversed, bool rightReversed, 
-                 okapi::AbstractMotor::gearset gearset):
+                 okapi::AbstractMotor::gearset gearset, float wD):
                  rightBase({okapi::Motor(rightMotor, rightReversed, 
                     gearset, okapi::AbstractMotor::encoderUnits::degrees)}),                 
                  leftBase({okapi::Motor(leftMotor, leftReversed, 
                     gearset, okapi::AbstractMotor::encoderUnits::degrees)})
 {
     /**
-     * The Chassis Constructor only needs to instantiate the
+     * The Chassis Constructor needs to instantiate the
      * rightBase and leftBase objects, which must be done through
-     * the constructor initializer list. Therefore, the
-     * constructor body is empty
+     * the constructor initializer list. Then, it sets the wheelDiameter variable
+     * to the passed in value
      */ 
+    wheelDiameter = wD;
 }
 
 Chassis::Chassis(int leftFrontMotor, int rightFrontMotor, int leftBackMotor, int rightBackMotor, 
                 bool leftFrontReversed, bool rightFrontReversed, bool leftBackReversed, bool rightBackReversed,
-                okapi::AbstractMotor::gearset gearset):
+                okapi::AbstractMotor::gearset gearset, float wD):
                  rightBase({okapi::Motor(rightFrontMotor, rightFrontReversed, 
                                 gearset, okapi::AbstractMotor::encoderUnits::degrees), 
                             okapi::Motor(rightBackMotor, rightBackReversed, 
@@ -35,14 +36,27 @@ Chassis::Chassis(int leftFrontMotor, int rightFrontMotor, int leftBackMotor, int
                                 gearset, okapi::AbstractMotor::encoderUnits::degrees)})
 {
     /**
-     * The Chassis Constructor only needs to instantiate the
+     * The Chassis Constructor needs to instantiate the
      * rightBase and leftBase objects, which must be done through
-     * the constructor initializer list. Therefore, the
-     * constructor body is empty
+     * the constructor initializer list. Then, it sets the wheelDiameter variable
+     * to the passed in value
      */ 
+    wheelDiameter = wD;
 }
 
 void Chassis::driver(okapi::Controller controller) {
+    /**
+     * The driver function uses the okapi controller object to get the values of
+     * the Y axes on each controller joystick. Then, each base motor group is set 
+     * to the value of its corresponding joystick. Since the getAnalog function
+     * returns a value between -1 and 1, the controllerSet function is used to 
+     * set the motors, as it accepts values in that range
+     */ 
     leftBase.controllerSet(controller.getAnalog(okapi::ControllerAnalog::leftY));
     rightBase.controllerSet(controller.getAnalog(okapi::ControllerAnalog::rightY));
+}
+
+void Chassis::chassisPID(float leftTarg, float rightTarg, int maxVelo)
+{
+    
 }
