ca3d9a97b0478dba79c423530c50ff1ec11cefc3
Nikhil Dave <nikhilrdave2003@gmail.com>
2020-08-10 06:51:46 -0700

    Scrapped current GUI code, will add Screen class

--- include/lib/gui.hpp
+++ include/lib/gui.hpp
@@ -2,6 +2,7 @@
 //The PROS Documentation instructs users to include the apix.h file if they want to use LVGL
 #include "pros/apix.h"
 #include <vector>
+#include <string>
 
 /**
  * The header file for the GUI class, which allows for the creation of an LVGL-based
@@ -12,11 +13,7 @@
 
 class GUI
 {
-
-
     public:
-        GUI(std::initializer_list<lv_obj_t*> scrs);
-
     private:
-        std::vector<lv_obj_t*> screens;
+
 };
