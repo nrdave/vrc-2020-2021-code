af0e48717d370cd6a98b266a395d71a7ab748d6d
Nikhil Dave <nikhilrdave2003@gmail.com>
2020-08-09 21:17:11 -0700

    Started work on gui header file

--- include/lib/gui.hpp
+++ include/lib/gui.hpp
@@ -1,6 +1,7 @@
 #pragma once
 //The PROS Documentation instructs users to include the apix.h file if they want to use LVGL
 #include "pros/apix.h"
+#include <vector>
 
 /**
  * The header file for the GUI class, which allows for the creation of an LVGL-based
@@ -11,4 +12,11 @@
 
 class GUI
 {
+
+
+    public:
+        GUI(std::initializer_list<lv_obj_t*> scrs);
+
+    private:
+        std::vector<lv_obj_t*> screens;
 };
