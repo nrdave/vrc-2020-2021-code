2df5c67baf7e955e7cbaef9a845abdfed8fb6ca7
Nikhil Dave <nikhilrdave2003@gmail.com>
2020-08-09 20:27:28 -0700

    Created gui header file

---
+++ include/lib/gui.hpp
@@ -0,0 +1,14 @@
+#pragma once
+//The PROS Documentation instructs users to include the apix.h file if they want to use LVGL
+#include "pros/apix.h"
+
+/**
+ * The header file for the GUI class, which allows for the creation of an LVGL-based
+ * GUI. LVGL, which stands for Little Video Graphics Library, is an open source C-based 
+ * graphics library. The PROS developers have configured all necessary LVGL settings
+ * in the PROS API files, so setting up a GUI is relatively simple.
+ */ 
+
+class GUI
+{
+};
