57b6d93519313aa50c1142b0e97755b6d662ee00
Nikhil Dave <nikhilrdave2003@gmail.com>
2020-11-16 14:33:01 -0700

    Added test autonomous code

--- src/autonomous.cpp
+++ src/autonomous.cpp
@@ -11,4 +11,8 @@
  * will be stopped. Re-enabling the robot will restart the task, not re-start it
  * from where it left off.
  */
-void autonomous() {}
+void autonomous() {
+    drive.moveStraight(5);
+    pros::delay(500);
+    drive.turnAngle(45);
+}
