338dff1ac38d1f8dffed75eb915ce453a01e2bfe
Nikhil Dave <nikhilrdave2003@gmail.com>
2021-02-10 14:29:59 -0700

    Starting to tune the drive PID loop

--- src/initialize.cpp
+++ src/initialize.cpp
@@ -1,6 +1,6 @@
 #include "main.h"
 
-TankDrive drive({13, 10} , {11, 3} , {false, false}, {true, true}, pros::E_MOTOR_GEARSET_18, 4, 12.75, 60, 0.05, 55);
+TankDrive drive({13, 10} , {11, 3} , {false, false}, {true, true}, pros::E_MOTOR_GEARSET_18, 4, 12.75, 80, 0, 0);
 Intake intake({18, 12}, {false, true}, pros::E_MOTOR_GEARSET_18, pros::E_CONTROLLER_DIGITAL_L1, pros::E_CONTROLLER_DIGITAL_L2);
 Conveyor conveyor({15}, {true}, pros::E_MOTOR_GEARSET_18, pros::E_CONTROLLER_DIGITAL_R1, pros::E_CONTROLLER_DIGITAL_R2);
 /**
