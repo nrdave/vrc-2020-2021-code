a16d1cfe68b622a69c7189ba90727544581e2783
Nikhil Dave <nikhilrdave2003@gmail.com>
2021-02-17 14:21:56 -0700

    drive PID tuning+ fixing ports

--- src/initialize.cpp
+++ src/initialize.cpp
@@ -1,6 +1,6 @@
 #include "main.h"
 
-TankDrive drive({13, 10} , {11, 3} , {false, false}, {true, true}, pros::E_MOTOR_GEARSET_18, 4, 12.75, 80, 0, 0);
+TankDrive drive({13, 10} , {3, 11} , {false, false}, {true, true}, pros::E_MOTOR_GEARSET_18, 4, 12.75, 27, 0, 0);
 Intake intake({18, 12}, {false, true}, pros::E_MOTOR_GEARSET_18, pros::E_CONTROLLER_DIGITAL_L1, pros::E_CONTROLLER_DIGITAL_L2);
 Conveyor conveyor({15}, {true}, pros::E_MOTOR_GEARSET_18, pros::E_CONTROLLER_DIGITAL_R1, pros::E_CONTROLLER_DIGITAL_R2);
 /**
